5a0c83116a6d7afed23fab4a1049a99d6faac6a8
Jon Cram <webignition@gmail.com>
2021-12-31 13:59:09 +0000

    Use php-cs-fixer (#13)

--- src/YamlGenerator.php
+++ src/YamlGenerator.php
@@ -9,11 +9,10 @@ use webignition\BasilRunnerDocuments\DocumentInterface;
 
 class YamlGenerator
 {
-    private const DOCUMENT_START = '---';
-    private const DOCUMENT_END = '...';
-
     public const DEFAULT_INLINE_DEPTH = 9;
     public const DEFAULT_INDENT_SIZE = 2;
+    private const DOCUMENT_START = '---';
+    private const DOCUMENT_END = '...';
 
     private int $inlineDepth = self::DEFAULT_INLINE_DEPTH;
     private int $indentSize = self::DEFAULT_INDENT_SIZE;
